9adb96a079c79246ef7fcfe4d891c0357a113be9
MorgantiWeb <info@morgantiweb.com>
2022-06-12 00:58:56 +0200

    Business card with Card and ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -17,51 +17,48 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black87,
           title: const Text('I am Rich'),
         ),
-        body: Column(children: <Widget>[
-          const CircleAvatar(
-              radius: 50.0, backgroundImage: AssetImage('images/test.jpg')),
-          const Text('Luca Morganti',
-              style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold)),
-          Text('FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20,
-                color: Colors.teal.shade100,
-                letterSpacing: 2.5,
-              )),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(Icons.phone, color: Colors.teal),
-                  SizedBox(width: 10.0),
-                  Text('+39 338 1234567', style: TextStyle(color: Colors.teal))
-                ],
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                  radius: 50.0, backgroundImage: AssetImage('images/test.jpg')),
+              const Text('Luca Morganti',
+                  style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Text('FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                  )),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100
+                )
               ),
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: const <Widget>[
-                  Icon(Icons.email, color: Colors.teal),
-                  SizedBox(width: 10.0),
-                  Text('morganti78@testemail.com', style: TextStyle(color: Colors.teal))
-                ],
+              const Card(
+                margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text('+39 338 1234567',
+                      style: TextStyle(color: Colors.teal)),
+                ),
               ),
-            ),
-          ),
-        ]),
+              const Card(
+                margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text('morganti78@testemail.com',
+                      style: TextStyle(color: Colors.teal)),
+                ),
+              ),
+            ]),
       ),
     );
   }
