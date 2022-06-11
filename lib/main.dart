8c20ba446fe6392e33e701fc4db7886b228c8035
MorgantiWeb <info@morgantiweb.com>
2022-06-11 23:56:13 +0200

    business card start

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,16 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black87,
           title: const Text('I am Rich'),
         ),
-        body: const Center(
-          child: const Image(
-              image: AssetImage(
-                  'images/test.jpg')),
-        ),
+        body: Column(children: const <Widget>[
+          CircleAvatar(
+              radius: 50.0, backgroundImage: AssetImage('images/test.jpg')),
+          Text('Luca Morganti',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              ))
+        ]),
       ),
     );
   }
